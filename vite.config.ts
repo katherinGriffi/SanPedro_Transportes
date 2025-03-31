9098a562fb4fee7d09e1684d461f8da2cd7b0c6d
katherinGriffi <88365422+katherinGriffi@users.noreply.github.com>
2025-03-31 17:16:10 +0100

    corrigiendo olvistadest contraena 7

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
-  
   plugins: [react()],
-  base: '/', // Adicione esta linha para desenvolvimento local
-  server: {
-    open: true, // Abre o navegador automaticamente
-  },
+  base: '/SanPedro_Transportes/', // Asegúrate que coincida con tu repo
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html') // Forza la inyección correcta
+      }
+    }
+  }
 });
